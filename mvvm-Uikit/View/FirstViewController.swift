1da920f0d0f6e46cdf29d3c3c38ad75e60abb6b8
Marcela Gonçalves <maazinhaa@gmail.com>
2022-06-06 20:08:01 -0300

    Descrição das alterações

---
+++ mvvm-Uikit/View/FirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FirstViewController.swift
+//  mvvm-Uikit
+//
+//  Created by Marcela Goncalves on 03/06/22.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mvvm-Uikit/ViewModel/FirstViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FirstViewModel.swift
+//  mvvm-Uikit
+//
+//  Created by Marcela Goncalves on 03/06/22.
+//
+
+import Foundation
